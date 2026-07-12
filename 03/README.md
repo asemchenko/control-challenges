09cc58f5301fe0ec90c7becd99f310508422c1e6
Andrii Semchenko <andriysemchenko@gmail.com>
2026-07-12 18:41:36 +0300

    Add solution for task 3

---
+++ 03/README.md
@@ -0,0 +1,1 @@
+https://github.com/user-attachments/assets/27fb80bf-ac3f-40fb-b579-81e8c358825e
---
+++ 03/solution.js
@@ -0,0 +1,25 @@
+let mistakeSum = 0;
+let lastMistake = 0;
+let lastT = 0;
+
+const targetX = 0;
+
+function controlFunction(block) {
+    const dT = block.T - lastT;
+    lastT = block.T;
+
+    // make PID controller
+    const p = targetX - block.x;
+    const i = mistakeSum * dT;
+    const d = (p - lastMistake) / dT;
+
+    mistakeSum += p;
+    lastMistake = p;
+
+    return 30 * p + 31 * tanh(p) * i + (7 + 1 * tanh(p)) * d;
+}
+
+function tanh(x) {
+    return 1 - Math.tanh(x);
+}
+
